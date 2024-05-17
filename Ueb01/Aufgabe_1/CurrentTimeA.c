91d4cfcfa3cbb8ed3605b26693e1bf170931c0a7
Christian Petry <christianpetry5@gmail.com>
2024-05-17 11:24:00 +0200

    Update

--- Ueb01/Aufgabe_1/CurrentTimeA.c
+++ Ueb01/Aufgabe_1/CurrentTimeA.c
@@ -3,8 +3,8 @@
 
 int main() {                                                        // Beginn der Funktion main
     time_t zeit;                                                    // Datentyp fuer Zeit, dargestellt mit Variable zeit
-    time(&zeit);                                                       // Ruft aktuelle Zeit auf und speichert sie in Variable zeit
+    time(&zeit);                                                    // Ruft aktuelle Zeit auf und speichert sie in Variable zeit
     printf("Aktuelle Zeit mit time(), gefolgt von ctime():\n");     // Ausgabe eines Strings
-    printf("%s", ctime(&zeit));                                        // Konvertiert Zeit in String und gibt sie aus, ctime(&zeit) gibt Pointer auf String zurueck
+    printf("%s", ctime(&zeit));                                     // Konvertiert Zeit in String und gibt sie aus, ctime(&zeit) gibt Pointer auf String zurueck
     return 0;                                                       // Programmende
 }
