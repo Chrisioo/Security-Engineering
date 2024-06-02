d43d9d9ef1d5f387dda1724b49289edbaa981d47
Christian Petry <christianpetry5@gmail.com>
2024-06-02 13:22:57 +0200

    Ueb02

--- Ueb02/Aufgabe 1/archivierung.sh
+++ Ueb02/Aufgabe 1/archivierung.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Check, ob ein Dateiname übergeben wurde
-if [ -z "$1" ]; then
+if [ -z "$1" ]; then                                # -z: Check, ob die Länge des Strings 0 ist
     echo "Es wird ein Dateiname benötigt"
     echo "Beispiel: ./archivierung.sh dateiname"
     exit 1
 fi
 
 # Check, ob die Datei existiert
-if [ ! -f $1 ]; then
+if [ ! -f $1 ]; then                                # -f: Check, ob die Datei existiert
     echo "Datei $1 wurde nicht gefunden"
     exit 1
 fi
