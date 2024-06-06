e599a4b0c5c25f7bbf559a5a7c6988e800c93fb1
Christian Petry <christianpetry5@gmail.com>
2024-06-06 07:49:04 +0200

    Update

--- Aufgabe 4/ausgabe.sh
+++ Aufgabe 4/ausgabe.sh
@@ -8,9 +8,9 @@ echo "Mit cut -c 17- wird der Inhalt ab dem 17. Zeichen ausgegeben."
 key=$(od -t x4 /dev/urandom | head -1 | cut -c 17- | sed -e "s/ //g")
 echo $key
 
-echo -e "\nAusgabe des Kommandos 'od -t x4 /dev/urandom | head -1 | cut -c 17-48 | sed -e "s/ //g"'"
-echo "Mit cut -c 17-48 wird der Inhalt von Zeichen 17 bis 48 ausgegeben."
-od -t x4 /dev/urandom | head -1 | cut -c 17-48 | sed -e "s/ //g"
+echo -e "\nAusgabe des Kommandos 'od -t x4 /dev/urandom | head -1 | cut -c 17-32 | sed -e "s/ //g"'"
+echo "Mit cut -c 17-32 wird der Inhalt von Zeichen 17 bis 32 ausgegeben."
+od -t x4 /dev/urandom | head -1 | cut -c 17-32 | sed -e "s/ //g"
 
 echo "Ausgabe des Kommandos 'openssl dgst -sha256 -mac HMAC -macopt hexkey:$key /etc/services'"
 echo "Hierbei wird der SHA256-Hashwert des Inhalts von /etc/services berechnet."
