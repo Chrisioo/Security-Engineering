1832bc163b5d37cdfa04704a86204f23f57a5d53
Christian Petry <christianpetry5@gmail.com>
2024-06-20 23:06:49 +0200

    Update

--- Aufgabe_3/Teil b/viewer.sh
+++ Aufgabe_3/Teil b/viewer.sh
@@ -8,13 +8,13 @@ fi
 file_type=$(file "$1")                                          # Dateiendung auslesen mit Kommando "file"
 
 if echo "$file_type" | grep -q "PDF document"; then             # Check, ob die Datei eine PDF-Datei ist
-    /usr/bin/evince "$1"                                        # Oeffnen mit evince
+    /usr/bin/evince "$1"                                        # Öffnen mit evince
 elif echo "$file_type" | grep -q "image"; then                  # Check, ob die Datei ein Bild ist
-    /usr/bin/xdg-open "$1"                                      # Oeffnen mit xdg-open
+    /usr/bin/eog "$1"                                           # Öffnen mit eog / Eye of Gnome
 elif echo "$file_type" | grep -q "text"; then                   # Check, ob die Datei eine Textdatei ist
-    /usr/bin/less "$1"                                          # Oeffnen mit less                                      
+    /usr/bin/less "$1"                                          # Öffnen mit less                                      
 elif echo "$file_type" | grep -q "OpenDocument"; then           # Check, ob die Datei eine OpenOffice-Datei ist
-    /usr/bin/libreoffice "$1"                                   # Oeffnen mit libreoffice                           
+    /usr/bin/libreoffice "$1"                                   # Öffnen mit libreoffice                           
 else
     echo "Unbekanntr Dateityp: $file_type"                      # Ausgabe, wenn die Dateiendung unbekannt ist
     exit 1                                                      # Exit mit Fehlercode 1
--- Aufgabe_3/Teil d/which_emulator.sh
+++ Aufgabe_3/Teil d/which_emulator.sh
@@ -1,26 +1,22 @@
 #!/bin/sh
 
-# Überprüfen, ob ein Argument übergeben wurde
-if [ "$#" -ne 1 ]; then
-    echo "Nutzung: $0 programname"
-    exit 1
+if [ "$#" -ne 1 ]; then                             # Überprüfen, ob ein Argument übergeben wurde
+    echo "Nutzung: $0 programname"                  # Fehlermeldung, wenn kein Argument übergeben wurde
+    exit 1                                          # Beenden des Skripts mit Fehlercode 1
 fi
 
-# Gesuchtes Programm
-program=$1
+program=$1                                          # Speichern des Programms in der Variablen "program"
 
-# Durchlaufen der in PATH definierten Verzeichnisse
-OLDIFS=$IFS
-IFS=':'
-for dir in $PATH; do
-    if [ -x "$dir/$program" ]; then
-        echo "$dir/$program"
-        IFS=$OLDIFS
-        exit 0
-    fi
-done
-IFS=$OLDIFS
+OLDIFS=$IFS                                         # Speichern des alten IFS
+IFS=':'                                             # Setzen des IFS auf ":"                 
+for dir in $PATH; do                                # Schleife über alle Verzeichnisse im PATH
+    if [ -x "$dir/$program" ]; then                 # Überprüfen, ob das Programm im Verzeichnis vorhanden ist
+        echo "$dir/$program"                        # Ausgabe des Pfades zum Programm
+        IFS=$OLDIFS                                 # Wiederherstellen des alten IFS
+        exit 0                                      # Beenden des Skripts mit Erfolg
+    fi                                              # Beenden der Schleife
+done                                                # Ende der Schleife
+IFS=$OLDIFS                                         # Wiederherstellen des alten IFS
 
-# Wenn das Programm nicht gefunden wurde
-echo "$program: Kommando nicht gefunden"
-exit 1
+echo "$program: Kommando nicht gefunden"            # Ausgabe einer Fehlermeldung, wenn das Programm nicht gefunden wurde
+exit 1                                              # Beenden des Skripts mit Fehlercode 1
