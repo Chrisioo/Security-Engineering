2b7e840a8df9d6f6a40d802e0f5c634ee820a188
Christian Petry <christianpetry5@gmail.com>
2024-06-24 18:33:08 +0200

    Comments

--- Aufgabe_3/Teil_d/which_emulator.sh
+++ Aufgabe_3/Teil_d/which_emulator.sh
@@ -1,22 +1,23 @@
-#!/bin/sh
+#!/bin/sh                                    # Specify the shell interpreter to use
 
-if [ "$#" -ne 1 ]; then                             # Check if an argument was given
-    echo "Usage: $0 <program name>"                 # Error message if no argument was given
-    exit 1                                          # Exit with error code 1
-fi                                                  # End of if statement
+if [ "$#" -ne 1 ]; then                      # Check if the number of arguments passed to the script is not equal to 1
+    echo "Usage: $0 <program name>"          # Print a usage message if the number of arguments is incorrect
+    exit 1                                   # Exit the script with a status code of 1 (indicating an error)
+fi                                           
 
-program=$1                                          # Program name is stored in the variable "program"
+program=$1                                   # Assign the first argument passed to the script to the variable 'program'
 
-OLDIFS=$IFS                                         # Storing the old IFS in the variable "OLDIFS"
-IFS=':'                                             # Setting the IFS to ":"                
-for dir in $PATH; do                                # Loop through all directories in the PATH
-    if [ -x "$dir/$program" ]; then                 # Überprüfen, ob das Programm im Verzeichnis vorhanden ist
-        echo "$dir/$program"                        # Ausgabe des Pfades zum Programm
-        IFS=$OLDIFS                                 # Wiederherstellen des alten IFS
-        exit 0                                      # Beenden des Skripts mit Erfolg
-    fi                                              # Beenden der Schleife
-done                                                # Ende der Schleife
-IFS=$OLDIFS                                         # Wiederherstellen des alten IFS
+OLDIFS=$IFS                                  # Save the current value of IFS (Internal Field Separator)
+IFS=':'                                      # Set IFS to ':' to split the PATH variable correctly
+for dir in $PATH; do                         # Iterate over each directory in the PATH variable
+    if [ -x "$dir/$program" ]; then          # Check if the program exists and is executable in the current directory
+        echo "$dir/$program"                 # Print the full path of the program
+        IFS=$OLDIFS                          # Restore the original IFS value
+        exit 0                               # Exit the script with a status code of 0 (indicating success)
+    fi                                       
+done                                         
 
-echo "$program: Command not found"                  # Ausgabe einer Fehlermeldung, wenn das Programm nicht gefunden wurde
-exit 1                                              # Beenden des Skripts mit Fehlercode 1
+IFS=$OLDIFS                                  # Restore the original IFS value (done here in case the loop completes without finding the program)
+
+echo "$program: Command not found"           # Print an error message if the program was not found in any directory in PATH
+exit 1                                       # Exit the script with a status code of 1 (indicating an error)
