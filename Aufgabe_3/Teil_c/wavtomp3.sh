f1770adf536f1daf98caa5fcb5f906ae8b9a60ab
Christian Petry <christianpetry5@gmail.com>
2024-06-25 08:08:18 +0200

    Update

--- Aufgabe_3/Teil_c/wavtomp3.sh
+++ Aufgabe_3/Teil_c/wavtomp3.sh
@@ -6,18 +6,13 @@ if ["$#" -lt 1]; then                               # Check if a file was given
 fi                                                  # End of if statement
 
 for wavfile in "$@"; do                             # Loop through all arguments
-    if [ ! -f "$wavfile" ]; then                    # Check if the file is a wav file
-        echo "Error: $wavfile is not a file"        # error message if the file is not a file
-        continue                                    # skip the rest of the loop
-    fi                                              # End of if statement
+    mp3file=$(basename "$wavfile" .wav)             # Get the base name of the file
+    ffmpeg -i "$wavfile" "$mp3file.mp3"             # Convert the wav file to mp3 using ffmpeg
 
-    base=$(basename "$wavfile" .wav)                # Get the base name of the file
-    ffmpeg -i "$wavfile" "$base.mp3"                # Convert the wav file to mp3 using ffmpeg
-
-    if [ $? -ne 0 ]; then                           # Check if the conversion was successful
+    if [ $? -ne 0 ]; then                           # Check if the conversion was successful by checking if the exit code of the previous command is not 0
         echo "Error converting $wavfile"            # error message if the conversion was not successful
     else 
-        echo "Converted $wavfile to $base.mp3"      # success message if the conversion was successful
+        echo "Converted $wavfile to $mp3file.mp3"   # success message if the conversion was successful
     fi                                              # End of if statement
 done                                                # End of for loop
 exit 0                                              # exit with success code 0
