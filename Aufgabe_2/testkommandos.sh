b7f59e58e418e16724168a147608e90749df45b8
Christian Petry <christianpetry5@gmail.com>
2024-06-30 16:58:19 +0200

    Aufgabe 1

--- Aufgabe_2/testkommandos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget -O fussball-tabelle.html https://de.wikipedia.org/wiki/Fu%C3%9Fball-Bundesliga_2023/24                             # https://de.wikipedia.org/wiki/Fussball-Bundesliga_2023/24 wird heruntergeladen
-sed -e "s:<th\>:<td\>:g" -e "s:</th>:</td>:g" <fussball-tabelle.html > fussball-tabelle2.html                           # <fussball-tabelle.html wird in <fussball-tabelle2.html umgewandelt, alle </th> werden durch </td> ersetzt
-diff -u fussball-tabelle.html fussball-tabelle2.html > diff.txt                                                         # <fussball-tabelle.html und <fussball-tabelle2.html werden verglichen, wird in diff.txt geschrieben
-egrep -n "mw-.*Abschlusstabelle" fussball-tabelle.html > egrep.txt                                                      # Die Zeile, in der die Bundesligatabelle beginnt, wird in egrep.txt geschrieben
-tail -n +$(egrep -n "mw-.*Abschlusstabelle" fussball-tabelle.html | cut -d: -f1) fussball-tabelle.html > tabelle.html   # Die Zeilen, die mit "mw-.*Abschlusstabelle" beginnen, werden in tabelle.html geschrieben
-fgrep "a href=" fussball-tabelle.html > links                                                                           # Alle Zeilen, die mit "a href=" beginnen, werden in links geschrieben
--- Aufgabe_3/Teil a/hallo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for i in $*             # $* gibt alle Argumente aus
-do                      # Schleife wird gestartet, durchläuft alle Argumente
-    echo "Hallo $i"     # $i gibt das aktuelle Argument aus, z.B. "Hallo Hans"
-done                    # Schleife wird beendet
-exit 0                  # Programm wird mit 0 beendet, d.h. keine Fehler
