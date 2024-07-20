1ed2d023b9522ebef9951bf9eb6c7690070de155
Christian Petry <christianpetry5@gmail.com>
2024-07-20 22:26:45 +0200

    Update  aufg 2

--- Aufgabe_2/bank_transaction.sh
+++ Aufgabe_2/bank_transaction.sh
@@ -19,21 +19,15 @@ while true; do
     echo "Bitte geben Sie Ihre TAN ein:"
     read USER_TAN
 
-    # Lesen des aktuellen Hashwerts
-    CURRENT_HASH=$(cat "$CURRENT_HASH_FILE")
-
-    # Berechnen des Hashwerts der eingegebenen TAN
-    USER_TAN_HASH=$(echo -n "$USER_TAN" | sha256sum | awk '{print $1}')
-
-    if grep -q "$USER_TAN_HASH" "$TAN_FILE"; then
+    if grep -q "$USER_TAN" "$TAN_FILE"; then
         # TAN gefunden und korrekt
         echo "Zugriff erlaubt."
 
         # Entfernen der verbrauchten TAN aus der TAN-Datei
-        sed -i "/$USER_TAN_HASH/d" "$TAN_FILE"
+        sed -i "/$USER_TAN/d" "$TAN_FILE"
 
         # Speichern des neuen aktuellen Hashwerts
-        echo "$USER_TAN_HASH" > "$CURRENT_HASH_FILE"
+        echo "$USER_TAN" > "$CURRENT_HASH_FILE"
     else
         echo "Zugriff verweigert: Falsche TAN oder TAN-Liste aufgebraucht."
     fi
