d359227fb28ba8ab1eef73d38a47ff4c4ea32acc
Christian Petry <christianpetry5@gmail.com>
2024-07-23 22:57:21 +0200

    Comment update

--- Aufgabe_1/cracker_demo.sh
+++ Aufgabe_1/cracker_demo.sh
@@ -6,42 +6,43 @@
 
 # User mit gehashten Passwörtern
 declare -A USERS
-USERS["Steffi.Jones"]='$1$O7v0C21Z$2FH7ib2Dxtoq6B83qTgON1'
-USERS["Marco.Reus"]='$1$Jebn3vQ5$2k..iqxtXNwfsCFAamWCS0'
-USERS["Birgit.Prinz"]='$1$7ieEwjFr$T/jwatbzqhLZNVDEfymB41'
+USERS["Steffi.Jones"]='$1$O7v0C21Z$2FH7ib2Dxtoq6B83qTgON1'                      # Ist in words_demo.txt vorhanden
+USERS["Marco.Reus"]='$1$Jebn3vQ5$2k..iqxtXNwfsCFAamWCS0'                        # Ist in words_demo.txt vorhanden
+USERS["Birgit.Prinz"]='$1$7ieEwjFr$T/jwatbzqhLZNVDEfymB41'                      # Ist nicht in words_demo.txt vorhanden (flyer)
 
 # Array zum Speichern der gefundenen Passwörter
 declare -A GEFUNDENE_PASSWOERTER
 
-while read -r word; do                                                      # Lese jedes Wort aus der Datei words.txt                       
-    for username in "${!USERS[@]}"; do                                      # Iteriere über alle User              
-        hash="${USERS[$username]}"                                          # Zuweisen des Hashes des aktuellen Users
-        salt=$(echo $hash | cut -d'$' -f3)                                  # Extrahieren des Salts aus dem Hash                  
-        target_hash=$(echo $hash | cut -d'$' -f4)                           # Extrahieren des Zielhashes aus dem Hash      
+while read -r word; do                                                          # Lese jedes Wort aus der Datei words.txt                       
+    for username in "${!USERS[@]}"; do                                          # Iteriere über alle User              
+        hash="${USERS[$username]}"                                              # Zuweisen des Hashes des aktuellen Users
+        salt=$(echo $hash | cut -d'$' -f3)                                      # Extrahieren des Salts aus dem Hash   
+                                                                                               
+        target_hash=$(echo $hash | cut -d'$' -f4)                               # Extrahieren des Zielhashes aus dem Hash      
 
-        generated_hash=$(openssl passwd -1 -salt "$salt" "$word")           # Generiere Hash für das aktuelle Wort
-        echo "Hash: $generated_hash, Wort: $word, salt: $salt ($username)"  # Ausgabe des generierten Hashes, des Wortes und des Salts
+        generated_hash=$(openssl passwd -1 -salt "$salt" "$word")               # Generiere Hash für das aktuelle Wort
+        echo "Hash: $generated_hash, Wort: $word, salt: $salt ($username)"      # Ausgabe des generierten Hashes, des Wortes und des Salts
         
-        if [[ "$generated_hash" == *"$target_hash"* ]]; then                # Vergleiche den generierten Hash mit dem Zielhash
-            echo "Passwort für Nutzer $username: $word"                     # Falls Passwort gefunden, Ausgabe des Nutzernamens und Passworts
-            GEFUNDENE_PASSWOERTER["$username"]="$word"                      # Speichern des gefundenen Passworts
-            unset USERS["$username"]                                        # Löschen des Nutzers aus dem Array, da Passwort gefunden
+        if [[ "$generated_hash" == *"$target_hash"* ]]; then                    # Vergleiche den generierten Hash mit dem Zielhash
+            echo "Passwort für Nutzer $username: $word"                         # Falls Passwort gefunden, Ausgabe des Nutzernamens und Passworts
+            GEFUNDENE_PASSWOERTER["$username"]="$word"                          # Speichern des gefundenen Passworts
+            unset USERS["$username"]                                            # Löschen des Nutzers aus dem Array, da Passwort gefunden
         fi
 
-        if [ ${#USERS[@]} -eq 0 ]; then                                     # Prüfe, ob alle Passwörter gefunden wurden
-            break 2                                                         # Beende die Schleife, falls alle Passwörter gefunden wurden
+        if [ ${#USERS[@]} -eq 0 ]; then                                         # Prüfe, ob alle Passwörter gefunden wurden
+            break 2                                                             # Beende die Schleife, falls alle Passwörter gefunden wurden
         fi
     done
 done < "words_demo.txt"
 
 # Ausgabe aller Nutzer mit ihren gefundenen Passwörtern
-for username in "${!GEFUNDENE_PASSWOERTER[@]}"; do
-    echo "Password für Nutzer $username: ${GEFUNDENE_PASSWOERTER[$username]}"
+for username in "${!GEFUNDENE_PASSWOERTER[@]}"; do                              # Durchlaufen von Array GEFUNDENE_PASSWOERTER         
+    echo "Password für Nutzer $username: ${GEFUNDENE_PASSWOERTER[$username]}"   # Ausgabe der gefundenen Passwörter mit Nutzernamen
 done
 
 # Ausgabe der Nutzer, für die keine Passwörter gefunden wurden
-for username in "${!USERS[@]}"; do
-    echo "Passwort für Nutzer $username wurde nicht gefunden."
+for username in "${!USERS[@]}"; do                                              # Durchlaufen von Array USERS
+    echo "Passwort für Nutzer $username wurde nicht gefunden."                  # Ausgabe übriger Nutzer (wird entfernt, wenn Passwort gefunden)
 done
 
 exit 0
