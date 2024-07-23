a5bcd863e551a9f9ab64fa2d890d8cd673985e39
Christian Petry <christianpetry5@gmail.com>
2024-07-24 01:38:06 +0200

    Update

--- Aufgabe_1/cracker_demo.sh
+++ Aufgabe_1/cracker_demo.sh
@@ -17,13 +17,11 @@ while read -r word; do                                                          
     for username in "${!USERS[@]}"; do                                          # Iteriere über alle User              
         hash="${USERS[$username]}"                                              # Zuweisen des Hashes des aktuellen Users
         salt=$(echo $hash | cut -d'$' -f3)                                      # Extrahieren des Salts aus dem Hash   
-                                                                                               
-        target_hash=$(echo $hash | cut -d'$' -f4)                               # Extrahieren des Zielhashes aus dem Hash      
 
         generated_hash=$(openssl passwd -1 -salt "$salt" "$word")               # Generiere Hash für das aktuelle Wort
         echo "Hash: $generated_hash, Wort: $word, salt: $salt ($username)"      # Ausgabe des generierten Hashes, des Wortes und des Salts
         
-        if [[ "$generated_hash" == *"$target_hash"* ]]; then                    # Vergleiche den generierten Hash mit dem Zielhash
+        if [[ "$generated_hash" == *"$hash"* ]]; then                    # Vergleiche den generierten Hash mit dem Zielhash
             echo "Passwort für Nutzer $username: $word"                         # Falls Passwort gefunden, Ausgabe des Nutzernamens und Passworts
             GEFUNDENE_PASSWOERTER["$username"]="$word"                          # Speichern des gefundenen Passworts
             unset USERS["$username"]                                            # Löschen des Nutzers aus dem Array, da Passwort gefunden
